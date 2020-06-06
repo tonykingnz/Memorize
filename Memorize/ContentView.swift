36ef2d6bf0d84bdd25ad051409bdad0b2a278d87
José Antônio Maria de Rezende Silva <joseant.silva@icloud.com>
2020-06-06 18:42:51 -0300

    Initial Commit

---
+++ Memorize/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Memorize
+//
+//  Created by José Antônio Maria de Rezende Silva on 6/6/20.
+//  Copyright © 2020 José Antônio Maria de Rezende Silva. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
