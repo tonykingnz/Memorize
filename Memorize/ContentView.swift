b3c0e1cb6630091f31fff2ed04bc40a88746a0ee
José Antônio Maria de Rezende Silva <joseant.silva@icloud.com>
2020-06-19 07:21:25 -0300

    Added basic Swift colors references, return commentary before Text and added space between the main code and the preview code.

--- Memorize/ContentView.swift
+++ Memorize/ContentView.swift
@@ -8,12 +8,41 @@
 
 import SwiftUI
 
+//Keywords is Magenta, Indentifiers is Green and Swift Indentifiers is Purple.
+
+/// The main view of the application
 struct ContentView: View {
     var body: some View {
-        Text("Hello, World!")
+        /*return*/ Text("Hello 😀, World!")
     }
 }
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/// #This code below is the what make the SwiftUI preview work:
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
