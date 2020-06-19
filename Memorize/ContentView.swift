c7f03053f6ecc88669b01b288f5533f58b049925
José Antônio Maria de Rezende Silva <joseant.silva@icloud.com>
2020-06-19 09:30:53 -0300

    Cleanered, comments removed. Comments can be check at lecture1 branch.

--- Memorize/ContentView.swift
+++ Memorize/ContentView.swift
@@ -8,12 +8,34 @@
 
 import SwiftUI
 
-//Keywords is Magenta, Indentifiers is Green and Swift Indentifiers is Purple.
-
-/// The main view of the application
 struct ContentView: View {
+
     var body: some View {
-        /*return*/ Text("Hello 😀, World!")
+        return HStack {
+            ForEach(0..<4) {index in
+                CardView(isFaceUp: true)
+            }
+        }
+        .padding()
+        .foregroundColor(Color.orange)
+        .font(Font.largeTitle)
+    }
+}
+
+struct CardView: View {
+    var isFaceUp: Bool
+    
+    var body: some View {
+        ZStack{
+            if isFaceUp {
+                RoundedRectangle(cornerRadius: 10.0).fill(Color.white)
+                RoundedRectangle(cornerRadius: 10.0).stroke(lineWidth: 3.0)
+                Text("👻")
+            }
+            else {
+                RoundedRectangle(cornerRadius: 10.0).fill()
+            }
+        }
     }
 }
 
@@ -22,27 +44,6 @@ struct ContentView: View {
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/// #This code below is the what make the SwiftUI preview work:
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
